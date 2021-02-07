38fe2283966c7a56fb011d5e9aaaf10fea7a8dfd
Aiyyskhan <aiyyskhan@gmail.com>
2021-02-07 18:18:25 +0900

    small changes

--- Assets/Scripts/PlayerNameInputField.cs
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -1,9 +1,10 @@
-using System.Collections;
 // using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+
 
 [RequireComponent(typeof(InputField))]
 public class PlayerNameInputField : MonoBehaviour
@@ -13,11 +14,42 @@ public class PlayerNameInputField : MonoBehaviour
     const string playerNamePrefKey = "PlayerName";
 
     #endregion
+
+    #region MonoBehaviour CallBacks
     
     void Start()
     {
-        
+        string defaultName = string.Empty;
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
     }
+
+    #endregion
+
+    #region Public Methods
+
+    public void SetPlayerName(string value){
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player name is null or empty");
+            return;
+        }
+
+        PhotonNetwork.NickName = value;
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+
+    #endregion
 
     // Update is called once per frame
     void Update()
