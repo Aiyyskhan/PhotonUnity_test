563fad77f0d19f147f443e97e3fabe4ec572988f
Aiyyskhan <aiyyskhan@gmail.com>
2021-02-07 16:05:56 +0900

    initial commit

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Private Serializable Fields
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created.")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    #endregion
+
+    #region Private Fields
+
+    /// <summary>
+    /// This client's version number. Users are separated from each
+    /// other by gameVersion (which allows you to make breaking changes).
+    /// </summary>
+    string gameVersion = "0.1";
+
+    void Awake(){
+        // 
+        // 
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Connect(){
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("OnConnectedToMaster() was called by PUN.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause){
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message){
+        Debug.Log("OnJoinRandomFailed() was called by PUN. No random room available, so we create one. \nCalling: PhotonNetwork.CreateRoom()");
+        PhotonNetwork.CreateRoom(null, new RoomOptions{ MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom(){
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
+
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
